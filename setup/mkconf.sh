3d4c4d78c123b1f864c0cd5bffce047cc18c5a3c
los-t <los-t@ya.ru>
2013-08-13 01:49:46 +0400

    Apply config, fix test

--- setup/mkconf.sh
+++ setup/mkconf.sh
@@ -13,7 +13,11 @@ CFGPATH=$HOMEPATH/prj/cfg/$MYNAME
 GITPATH=git@github.com:los-t/$MYNAME.git
 
 echo "Cloning self to $CFGPATH"
-git clone $GITPATH $CFGPATH/$MYNAME; RET=$?;
+git clone $GITPATH $CFGPATH; RET=$?;
+
+ln -vs $CFGPATH/cfg/_profile $HOMEPATH/.profile
+ln -vs $CFGPATH/cfg/_bashrc $HOMEPATH/.bashrc
+ln -vs $CFGPATH/cfg/_vimrc $HOMEPATH/.vimrc
 
 
 if [ $RET -eq 0 ]; then
