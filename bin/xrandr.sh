c58d74a5ac66754e6ea862920411b92417917d07
Vladimir Dranyonkov <los-t@ya.ru>
2013-09-26 14:53:20 +0400

    Add basic X configuration

---
+++ bin/xrandr.sh
@@ -0,0 +1,5 @@
+# Add your hardware specific output configuration here.
+
+# Real life example:
+# xrandr --output DVI-0 --left-of DisplayPort-1
+# xrandr --output HDMI-0 --left-of DVI-0
--- setup/mkconf.sh
+++ setup/mkconf.sh
@@ -20,8 +20,12 @@ ln -vs $CFGPATH/cfg/_profile $HOMEPATH/.profile
 ln -vs $CFGPATH/cfg/_bashrc $HOMEPATH/.bashrc
 ln -vs $CFGPATH/cfg/_zshenv $HOMEPATH/.zshenv
 ln -vs $CFGPATH/cfg/_zshrc $HOMEPATH/.zshrc
-ln -vs $CFGPATH/cfg/_Xresources $HOMEPATH/.Xresources
 ln -vs $CFGPATH/cfg/_gitconfig $HOMEPATH/.gitconfig
+
+ln -vs $CFGPATH/cfg/_Xresources $HOMEPATH/.Xresources
+cp -v $CFGPATH/bin/xrandr.sh $HOMEPATH/bin/xrandr.sh
+ln -vs $CFGPATH/cfg/_xinitrc $HOMEPATH/.xinitrc
+echo "NOTE: Remember to add proper lines to ~/bin/xrandr.sh"
 
 mkdir -vp $XDGCFGPATH/i3
 ln -vs $CFGPATH/cfg/i3/config $XDGCFGPATH/i3/config
